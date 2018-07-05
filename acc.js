e26d24084499ab5b0a200075fc4d28f6d6b1cebe
trabajosig <37843964+trabajosig@users.noreply.github.com>
2018-07-05 12:38:28 -0400

    Add files via upload

---
+++ acc.js
@@ -0,0 +1,14 @@
+var acc = {
+"type": "FeatureCollection",
+"name": "acc",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0, "Nombre": "Ventana" }, "geometry": { "type": "LineString", "coordinates": [ [ -69.602460531840421, -18.263234180751226 ], [ -69.602453432998928, -18.263236730932935 ] ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nombre": "Ventana" }, "geometry": { "type": "LineString", "coordinates": [ [ -69.602386931653953, -18.263148776536649 ], [ -69.602389284862511, -18.26315491519134 ] ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nombre": "Ventana" }, "geometry": { "type": "LineString", "coordinates": [ [ -69.602355582970205, -18.26306737960126 ], [ -69.602357632363592, -18.263073778213627 ] ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nombre": "Ventana" }, "geometry": { "type": "LineString", "coordinates": [ [ -69.602248407642875, -18.263092962575978 ], [ -69.602255440082274, -18.263090725746626 ] ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nombre": "Acesso" }, "geometry": { "type": "LineString", "coordinates": [ [ -69.602241194382628, -18.263120015155703 ], [ -69.602244818456342, -18.26313049166772 ] ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nombre": "Ventana" }, "geometry": { "type": "LineString", "coordinates": [ [ -69.602268499652652, -18.263179524966372 ], [ -69.602270924988915, -18.263186287032184 ] ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nombre": "Acesso" }, "geometry": { "type": "LineString", "coordinates": [ [ -69.602335442305417, -18.263229590696092 ], [ -69.602342180367657, -18.263246742177035 ] ] } }
+]
+}
